58a84a93a4691a29be92f45d4dacafbcf32cecef
Chris Hill <chrishillbuilds@gmail.com>
2019-10-01 17:40:03 -0400

    add apikey.js to index.html and movies to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,9 @@ const musicCardTemplate = `
     <div data-index="<%= id %>" class="small-card music-card">
         <img class="album-art" src="<%= art %>">
         <div class="song-info">
-            <h2><%= track%></h2>
-            <h3><%= artist%></h3>
-            <h3><i><%= album%></i></h3>
+            <h2><%= track %></h2>
+            <h3><%= artist %></h3>
+            <h3><i><%= album %></i></h3>
         </div>
         <div class="upvoteDownvote">
             <img class="voteButton" src="images/thumbs-up-hand-symbol.svg">
@@ -19,14 +19,36 @@ const musicCardTemplate = `
         </div>
     </div>`;    
 
-// create a new template function with your html
+//  template html
+const movieCardTemplate = `
+    <div data-index="<%= id %>" class="small-card music-card">
+        <img class="album-art" src="<%= art %>">
+        <div class="song-info">
+            <h2><%= title %></h2>
+            <h3><%= genre %></h3>
+            <h3><i><%= date %></i></h3>
+        </div>
+        <div class="upvoteDownvote">
+            <img class="voteButton" src="images/thumbs-up-hand-symbol.svg">
+            <img class="voteButton" src="images/thumbs-down-silhouette.svg">
+        </div>
+    </div>`;    
+
+
+// create a new template function with your html for music
 const musicTemplateFn = _.template(musicCardTemplate);
 
+// create a new template function with your html for movies
+const movieTemplateFn = _.template(movieCardTemplate);
 
-// call the template function, passing it data. This returns compiled html
-function createMusicList(response) {
-    let musicArray = _.reverse(response.trending);
-    musicArray.forEach(
+
+// call the music template function, passing it data. This returns compiled html
+function createMusicList(responseMusic) {
+    let musicArray = _.reverse(responseMusic.trending);
+    console.log(musicArray);
+    let musicArray4 = musicArray.splice(0,4);
+    console.log(musicArray4);
+    musicArray4.forEach(
         (item) => {
             //let item = musicArray[musicArray.length - index - 1]
         //console.log(item.intChartPlace);    
@@ -40,18 +62,52 @@ function createMusicList(response) {
     )
 }
 
+// call the movies template function, passing it data. This returns compiled html
+function createMoviesList(responseMovies) {
+    let movieArray = responseMovies.results;
+    console.log(movieArray);
+    let movieArray4 = movieArray.splice(0,10);
+    console.log(movieArray4);
+    movieArray4.forEach(
+        (item) => {
+            //let item = musicArray[musicArray.length - index - 1]
+        console.log(item.title);    
+        console.log(item.genre_ids[0]);
+        console.log(item.poster_path);
+        console.log(item.release_date);
+        console.log(item.id);
+            let html = movieTemplateFn({id: item.id, title: item.title, genre: item.genre_ids[0], date: item.release_date, art: "https://image.tmdb.org/t/p/w200" + item.poster_path});
+            main.append(html)
+        }
+    )
+}
 
 //create a function to retrieve trending music data
-function updateTrendData() {
+function updateTrendMusicData() {
     
     get(`https://theaudiodb.com/api/v1/json/1/trending.php?country=us&type=itunes&format=singles&country=us&type=itunes&format=singles`)
-    .then(response => {
-        createMusicList(response);
+    .then(responseMusic => {
+        createMusicList(responseMusic);
         //console.log("response is ", response);
     });
           
-updateTrendData();
 }
+
+updateTrendMusicData();
+
+//create a function to retrieve movie data
+function updateMovieData() {
+    
+    get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${moviesKey}`)
+    .then(responseMovies => {
+        createMoviesList(responseMovies);
+        //console.log("response is ", responseMovies);
+        //console.log("response is ", responseMovies.results.title);
+    });
+          
+}
+
+updateMovieData();
     
 
 
