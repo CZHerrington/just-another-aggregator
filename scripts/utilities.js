ff7378f8790d3d16848b5954fa8820512a79cbf1
Christopher Herrington <christopher.herrington@outlook.com>
2019-10-01 23:32:40 -0400

    removes console.log calls and fixes toggleDefaultCategory() bug

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -30,50 +30,42 @@ class Api {
         this.name = name;
         this.prefs = emptyPrefs;
 
-        console.log('init Api()')
         get(apiUrlCreator(indexId))
             .then((json) => {this.binIdMap = json})
-            .then(() => console.log("got binIdMap: ", this.binIdMap))
             .then(() => {this.requestPrefs(name)})
     }
 
     requestPrefs(name) {
-        if (this.binIdMap === {}) console.warn('ONLY CALL ._requestPrefs() AFTER binIdMap IS POPULATED')
+        if (this.binIdMap === {}) console.warn('ONLY CALL .requestPrefs() AFTER binIdMap IS POPULATED -- OTHERWISE `name` WILL BE OVERWRITTEN')
         if (!this.binIdMap[name]) {
-            console.log('_requestPrefs can\'t find ' + name)
             this.prefs = emptyPrefs;
             this._addName(name);
         } else {
-            console.log(name + ' exists, returning prefs');
             const id = this.binIdMap[name];
-            // nocache is to avoid browser 304 responses
             // fetch prefs
+            // nocache is to avoid browser 304 responses
             sendRequest(apiUrlCreator(id), 'GET')
                 .then((json) => {this.prefs = json})
-                .then((json) => console.log('got prefs', this.prefs))
         }
     }
 
     _addName(name) {
-        console.log('adding name', name)
         //  create new id and prefs for user
         this.prefs.name = name;
         this.name = name;
         sendRequest(apiUrl, "POST", this.prefs)
             .then((json) => {
-                console.log('new id ', json);
                 this.binIdMap[name] = json.id;
                 sendRequest(apiUrlIndex, "PUT", this.binIdMap)
                 this.persistPrefs()
             })
     }
     persistPrefs() {
-        console.log('persist prefs', this.prefs);
         const id = this.binIdMap[this.name];
         if (id !== undefined) {
             sendRequest(apiUrlCreatorCache(id), "PUT", this.prefs)
         } else {
-            console.log('name undefined ', this.binIdMap)
+
         }
     }
 
@@ -104,7 +96,7 @@ class Api {
         if (index === -1) {
             this.prefs.data.defaultCategories.push(category);
         } else {
-            delete this.prefs.data.defaultCategories.splice(index, 1);
+            this.prefs.data.defaultCategories.splice(index, 1);
         }
     }
 }
